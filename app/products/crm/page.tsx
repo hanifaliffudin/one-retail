0425fefe709a0b83dc33fa2e0d2771a3f197d804
Hanif Aliffudin <68997219+hanifaliffudin@users.noreply.github.com>
2023-09-03 22:59:43 +0700

    page crm

--- app/products/crm/page.tsx
+++ app/products/crm/page.tsx
@@ -244,7 +244,7 @@ const CRMPage = () => {
               </p>
             </div>
 
-            <div className="flex justify-center mt-[419px]">
+            <div className="flex justify-center mt-[419px] mb-20">
               <div className="w-[557px] text-center">
                 <h2 className="text-white font-bold sm:text-[36px] text-xl sm:leading-[54px] mb-3">
                   Level-Up Engagement with Gamification
@@ -256,6 +256,157 @@ const CRMPage = () => {
                 </p>
               </div>
             </div>
+
+            <div className="flex items-center justify-center gap-x-[59px] mb-[65px]">
+              <img src="/products/crm/levelup-1.svg" alt="level up" />
+              <div className="w-[383px]">
+                <h3 className="font-bold text-neutral-n-10 mb-4 text-2xl">
+                  Engaging Game-Like Elements
+                </h3>
+                <p className="text-neutral-n-50">
+                  Integrate exciting features like Gacha, Spin the Wheel, Lucky
+                  Draw, and Duck Hunt into your loyalty app. Create a
+                  captivating experience that keeps users entertained and
+                  motivated to participate.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center gap-x-[59px] mb-[65px]">
+              <div className="w-[383px]">
+                <h3 className="font-bold text-neutral-n-10 mb-4 text-2xl">
+                  Customized Games
+                </h3>
+                <p className="text-neutral-n-50">
+                  Design and develop unique games tailored to your brand and
+                  target audience. Enhance user engagement by offering
+                  interactive and enjoyable gaming experiences within your
+                  loyalty app.
+                </p>
+              </div>
+              <img src="/products/crm/levelup-2.svg" alt="level up" />
+            </div>
+
+            <div className="flex items-center justify-center gap-x-[59px] mb-[65px]">
+              <img src="/products/crm/levelup-3.svg" alt="level up" />
+              <div className="w-[383px]">
+                <h3 className="font-bold text-neutral-n-10 mb-4 text-2xl">
+                  Boost Customer Participation
+                </h3>
+                <p className="text-neutral-n-50">
+                  Encourage active participation and repeated usage of your
+                  loyalty app through gamification. Increase customer
+                  engagement, loyalty, and satisfaction by making your loyalty
+                  program more fun and interactive.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="sm:px-24 mb-[100px]">
+          <h2 className="font-bold sm:text-[36px] text-xl sm:leading-[54px] mb-3">
+            E-Commerce
+          </h2>
+          <p className="text-gray-600 mb-[58px] max-w-[555px]">
+            Empower your online business with our powerful One Retail e-commerce
+            platform for seamless selling, attracting customers, and increasing
+            sales.
+          </p>
+
+          <div className="grid grid-cols-2 gap-x-10">
+            <div className="col-span-2 sm:col-span-1">
+              <div className="h-[593px] relative bg-[#152B74] py-10 px-[49px] rounded-3xl">
+                <div className="w-[455px]">
+                  <h3 className="text-white mb-4 font-bold text-2xl">
+                    Business 2 Business
+                  </h3>
+                  <p className="text-neutral-n-50">
+                    Business to Business (B2B) E-commerce support selling
+                    process between businesses. B2B provide order and payment
+                    report for business to track the overall sales data and
+                    activity, these all features can lead your business to get
+                    customer long term relation and repeat bulk purchases
+                  </p>
+                </div>
+                <img
+                  className="absolute left-0 bottom-0 rounded-bl-3xl"
+                  src="/products/crm/bg-b2b.svg"
+                  alt="ecommerce 1"
+                />
+                <img
+                  className="absolute right-0 bottom-0 rounded-br-3xl"
+                  src="/products/crm/ecommerce-1.svg"
+                  alt="ecommerce 1"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-2 sm:col-span-1">
+              <div className="h-[593px] relative bg-[#0A153A] py-10 px-[49px] rounded-3xl">
+                <div className="absolute left-[42px] bottom-[27px] w-[469px]">
+                  <h3 className="text-white mb-4 font-bold text-2xl">
+                    Business 2 Business
+                  </h3>
+                  <p className="text-neutral-n-50">
+                    Business to Business (B2B) E-commerce support selling
+                    process between businesses. B2B provide order and payment
+                    report for business to track the overall sales data and
+                    activity, these all features can lead your business to get
+                    customer long term relation and repeat bulk purchases
+                  </p>
+                </div>
+                <img
+                  className="absolute left-0 top-0 rounded-tl-3xl"
+                  src="/products/crm/bg-b2c.svg"
+                  alt="ecommerce 2"
+                />
+                <img
+                  className="absolute right-0 top-0 rounded-tr-3xl"
+                  src="/products/crm/ecommerce-2.svg"
+                  alt="ecommerce 2"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="grid grid-cols-2 sm:px-24 gap-x-[90px] mb-[54px]">
+          <div className="col-span-2 sm:col-span-1">
+            <img className="h-full" src="/products/crm/pim.svg" alt="pim" />
+          </div>
+          <div className="col-span-2 sm:col-span-1 max-w-[488px]">
+            <h2 className="mb-3 font-bold sm:text-[36px] text-xl sm:leading-[54px]">
+              Centralize Product Data with Product Information Management (PIM)
+            </h2>
+            <p className="text-neutral-n-90 mb-12">
+              Effortlessly centralize and update complex product data with One
+              Retail PIM. Streamline your operations, improve collaboration, and
+              deliver exceptional customer experiences with real-time, automated
+              communication.
+            </p>
+            <div className="flex items-center gap-x-3 mb-4">
+              <img src="/products/crm/check-circle.svg" alt="check" />
+              <p className="font-bold text-xl text-neutral-n-100">
+                Organize all your product in one place
+              </p>
+            </div>
+            <div className="flex items-center gap-x-3 mb-4">
+              <img src="/products/crm/check-circle.svg" alt="check" />
+              <p className="font-bold text-xl text-neutral-n-100">
+                Organize all your product in one place
+              </p>
+            </div>
+            <div className="flex items-center gap-x-3">
+              <img src="/products/crm/check-circle.svg" alt="check" />
+              <p className="font-bold text-xl text-neutral-n-100">
+                Organize all your product in one place
+              </p>
+            </div>
           </div>
         </div>
       </section>
@@ -263,19 +414,14 @@ const CRMPage = () => {
       <FAQ />
 
       <section>
-        <div className="rounded-[20px] relative bg-[#1A3591] sm:mx-24 p-10 mb-[152px]">
-          <img
-            className="absolute right-0 top-0 rounded-[20px]"
-            src="/products/oa/Hole.svg"
-            alt="Hole"
-          />
-          <div className="grid grid-cols-12">
+        <div className="relative cta-crm mb-20">
+          <div className="grid grid-cols-12 sm:px-[160px]">
             <div className="col-span-12 sm:col-span-6 text-white sm:max-w-[503px] flex items-center">
-              <div className="">
-                <h3 className="font-bold sm:text-[32px] text-xl sm:leading-[48px]">
-                  Experience Exceptional Support: Get in Touch Today!
+              <div className="max-w-[370px]">
+                <h3 className="font-bold sm:text-[32px] text-xl sm:leading-[48px] mb-2">
+                  Get in Touch Today!
                 </h3>
-                <p className="text-[#EDEDED] my-4">
+                <p className="text-[#EDEDED] mb-8">
                   Reach Out and Experience Our Stellar Customer Support: We're
                   Here to Serve You!
                 </p>
@@ -290,15 +436,20 @@ const CRMPage = () => {
               </div>
             </div>
             <div className="col-span-12 sm:col-span-6">
-              <div className="relative w-[565px] h-[329px]">
+              <div className="relative h-[369px] w-full">
                 <img
-                  className="absolute right-0 top-0 z-10"
-                  src="/products/oa/cta-1.svg"
+                  className="absolute right-20 -top-[45px] rounded-[20px]"
+                  src="/products/crm/circle-white.svg"
+                  alt="circle-white"
+                />
+                <img
+                  className="absolute right-[58px] top-[25px] z-10"
+                  src="/products/crm/cta-1.svg"
                   alt="cta-1"
                 />
                 <img
-                  className="absolute left-0 bottom-0 z-20"
-                  src="/products/oa/cta-2.svg"
+                  className="absolute -right-7 bottom-[20px] z-20"
+                  src="/products/crm/cta-2.svg"
                   alt="cta-2"
                 />
               </div>
